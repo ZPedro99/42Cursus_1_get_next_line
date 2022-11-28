d0c31d59292cbf8a6416f4c8451260e2277de993
José Gonçalves <118729517+ZPedro99@users.noreply.github.com>
2022-11-28 14:01:13 +0000

    Add files via upload

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jomirand <jomirand@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 14:50:20 by jomirand          #+#    #+#             */
+/*   Updated: 2022/11/25 17:58:54 by jomirand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# ifndef MAX_F_OPEN
+#  define MAX_F_OPEN 1024
+# endif
+
+char	*ft_strjoin(char const *str1, char const *str2);
+char	*ft_strchr(const char *str, int c);
+size_t	ft_strlen(const char *str);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*get_next_line(int fd);
+
+#endif
